9e7d729061dd2f02b6241ace5e0f3dfbee93621e
upasana27 <31506763+upasana27@users.noreply.github.com>
2018-02-16 08:44:21 +0530

    Update motor.py

--- motor.py
+++ motor.py
@@ -31,22 +31,18 @@ while(true)
       GPIO.output(Motor2B,GPIO.LOW)
       GPIO.output(Motor2E,GPIO.HIGH)
       sleep(10)
-      
-      }
-  if(in=='x')
-    {print "Going backwards"
-    GPIO.output(Motor1A,GPIO.LOW)
-    GPIO.output(Motor1B,GPIO.HIGH)
-    GPIO.output(Motor1E,GPIO.HIGH)
- 
-    GPIO.output(Motor2A,GPIO.LOW)
-    GPIO.output(Motor2B,GPIO.HIGH)
-    GPIO.output(Motor2E,GPIO.HIGH)
-    sleep(10)}
-  if(in=='w')
+    }
+    
+  elif(in=="s")
     {print "Now stop"
-    GPIO.output(Motor1E,GPIO.LOW)
-    GPIO.output(Motor2E,GPIO.LOW)}
+    GPIO.output(Motor1A,GPIO.LOW)
+     GPIO.output(Motor1B,GPIO.LOW)
+     GPIO.output(Motor1E,GPIO.HIGH)
+ 
+      GPIO.output(Motor2A,GPIO.LOW)
+      GPIO.output(Motor2B,GPIO.LOW)
+      GPIO.output(Motor2E,GPIO.HIGH)
+      sleep(2)}
     
  
 GPIO.cleanup()
